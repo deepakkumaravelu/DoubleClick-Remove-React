4c2e49d276358d45038e82ee9f416adc0f14f520
deepakkumaravelu <deepak1122003kumar@gmail.com>
2023-10-13 11:39:15 +0530

    doubleclick updated

--- doubleclickremove/src/components/DoubleClickRemove.jsx
+++ doubleclickremove/src/components/DoubleClickRemove.jsx
@@ -1,19 +1,18 @@
 import { useState } from "react"
 
 
+export default function DoubleClick(){
+    const array=[{id:0,name:"Leo"},{id:1,name:"Vikram"},{id:2,name:"Kaithi"},{id:3,name:"Rolex"}];
+    const [item,setitem]=useState(array);
+    return(
+        <div>
+            <h1>Click to delete</h1>
+            <ul class="lists">
+                {item.map(it=><li onClick={()=>{it.id=it.id+1; setitem(item.filter(f=>(f.id!==2)))}} >
+                         
+                    {it.name}<br></br><p>Count:{it.id}</p></li>)}
+            </ul>
 
-const DoubleClickRemove = () => {
-    const arr=[{nam:"hi"},{nam:"hello"},{nam:"vanakaam"}]
-  return (
-    <div>
-    
-    <ul>
-    {arr.map((val)=>{
-<li>{val.nam}</li>
-    })}
-    <li>hello</li>
-    </ul>
-    
-    </div>
-  )
+        </div>
+    )
 }
